01f6e02ef4decce0e0f6deab6c757209a6762915
Eugie Limpin <eugiellimpin@gmail.com>
2015-05-14 15:10:35 +0800

    Implement create action to add a shop
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,4 +2,15 @@ class ShopsController < ApplicationController
   def new
     @shop = Shop.new
   end
+
+  def create
+    @shop = Shop.new(shop_params)
+    @shop.save
+    redirect_to @shop
+  end
+
+  private
+    def shop_params
+      params.require(:shop).permit(:name, :description)
+    end
 end
