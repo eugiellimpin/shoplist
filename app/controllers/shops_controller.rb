509c24b11f5695338680ee365432b1e4fb526a12
Eugie Limpin <eugiellimpin@gmail.com>
2015-05-20 14:46:48 +0800

    Implement edit shop
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -11,10 +11,24 @@ class ShopsController < ApplicationController
     @shop = Shop.new
   end
 
+  def edit
+    @shop = Shop.find(params[:id])
+  end
+
   def create
     @shop = Shop.new(shop_params)
     @shop.save
     redirect_to @shop
+  end
+
+  def update
+    @shop = Shop.find(params[:id])
+
+    if @shop.update(shop_params)
+      redirect_to @shop
+    else
+      render 'edit'
+    end
   end
 
   private
