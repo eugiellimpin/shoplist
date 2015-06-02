44633d988f0197c10c25cec0566fb192ab5d62ab
Eugie Limpin <eugiellimpin@gmail.com>
2015-06-02 22:23:33 +0800

    Validate that at least one (website, FB or IG) is provided
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,13 @@
 class Shop < ActiveRecord::Base
+  mount_uploader :cover, ImageUploader
+
   validates :name, presence: true
   validates :description, length: { maximum: 140 }
-  mount_uploader :cover, ImageUploader
+  validate :at_least_one_social_media
+
+  def at_least_one_social_media
+    if website.blank? && facebook.blank? && instagram.blank?
+      errors[:website] << ("please provide at least one")
+    end
+  end
 end
