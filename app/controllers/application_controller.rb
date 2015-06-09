150b0f757af923d24c6f381e06a9ba1de23cad5e
Eugie Limpin <eugiellimpin@gmail.com>
2015-06-09 19:41:46 +0800

    Display message instead of CanCan::AccessDenied exception
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    @shops = Shop.all
+    flash[:notice] = 'Yo! That feature is not yet available. Please contact eugiellimpin@gmail.com for more details. Thanks!'
+    render 'shops/index'
+  end
 end
