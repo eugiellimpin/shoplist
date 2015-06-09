127c020b8c7ddf5481714fa5e1ae680fc34e206c
Eugie Limpin <eugiellimpin@gmail.com>
2015-06-09 19:49:26 +0800

    Ensure authorization for all controllers except devise
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # ensure authorization happens on every action in the app
+  check_authorization :unless => :devise_controller?
+
   rescue_from CanCan::AccessDenied do |exception|
     @shops = Shop.all
     flash[:notice] = 'Yo! That feature is not yet available. Please contact eugiellimpin@gmail.com for more details. Thanks!'
